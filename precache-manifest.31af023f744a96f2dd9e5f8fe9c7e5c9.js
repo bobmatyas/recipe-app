825453437b849d493a5aec9a8874bdda2db5bfda
bobmatyas <ballium@protonmail.com>
2019-10-12 08:24:13 -0400

    Updates

---
+++ precache-manifest.31af023f744a96f2dd9e5f8fe9c7e5c9.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96413b0b8d57ade31d013e30aedefe51",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "334400bc9f711588ec7b",
+    "url": "/recipe-app/static/css/main.bde39556.chunk.css"
+  },
+  {
+    "revision": "18b44bd92b8e546e717d",
+    "url": "/recipe-app/static/js/2.2ad02e3b.chunk.js"
+  },
+  {
+    "revision": "334400bc9f711588ec7b",
+    "url": "/recipe-app/static/js/main.2cf17002.chunk.js"
+  },
+  {
+    "revision": "4913b77c6635ff050728",
+    "url": "/recipe-app/static/js/runtime-main.8d63ef22.js"
+  },
+  {
+    "revision": "9ef40c43cdb3f3db0825d697074458aa",
+    "url": "/recipe-app/static/media/fact.9ef40c43.svg"
+  },
+  {
+    "revision": "d3971c339ee220a235f01eff50b12454",
+    "url": "/recipe-app/static/media/icon-alcohol.d3971c33.svg"
+  },
+  {
+    "revision": "8450c46b669a2f1c2850539ba6052e21",
+    "url": "/recipe-app/static/media/icon-check.8450c46b.svg"
+  },
+  {
+    "revision": "add7cd604bcf0d36b8ffa0808808ffdf",
+    "url": "/recipe-app/static/media/icon-cooking.add7cd60.svg"
+  },
+  {
+    "revision": "4dc9fe792b1e6e6038349a9d966939a7",
+    "url": "/recipe-app/static/media/icon-nut.4dc9fe79.svg"
+  },
+  {
+    "revision": "ee831d38ffc5f47906c03f51ae2ed63d",
+    "url": "/recipe-app/static/media/icon-peanuts.ee831d38.svg"
+  },
+  {
+    "revision": "9f22b6591e5511bd27a84eabbc62b044",
+    "url": "/recipe-app/static/media/icon-sugar.9f22b659.svg"
+  },
+  {
+    "revision": "ef868f257d8e93d5aa0e37914f92af5f",
+    "url": "/recipe-app/static/media/icon-vegan.ef868f25.svg"
+  },
+  {
+    "revision": "a32fcd82a8ebac1fb78148e9e2b80ce1",
+    "url": "/recipe-app/static/media/icon-vegetarian.a32fcd82.svg"
+  },
+  {
+    "revision": "4a00cc69451dc3be5c8529700320b477",
+    "url": "/recipe-app/static/media/ingredients.4a00cc69.svg"
+  },
+  {
+    "revision": "7d74d54efa34c6335b0c9b4249750f8a",
+    "url": "/recipe-app/static/media/logo-vegan.7d74d54e.svg"
+  },
+  {
+    "revision": "84e81ffe9ebbb13d1dc8be5b91ad819d",
+    "url": "/recipe-app/static/media/search-bg-01.84e81ffe.jpg"
+  },
+  {
+    "revision": "9a25eded2ef4c45ffdbeab230c5a6d6d",
+    "url": "/recipe-app/static/media/search-bg-02.9a25eded.jpg"
+  },
+  {
+    "revision": "c21e6953e231da4c2415acfc2d20de05",
+    "url": "/recipe-app/static/media/search-bg-03.c21e6953.jpg"
+  },
+  {
+    "revision": "a4672f27ea5559153a87514aef095fe6",
+    "url": "/recipe-app/static/media/search-bg-04.a4672f27.jpg"
+  },
+  {
+    "revision": "d36f55329f82905a74f5d04186f9c8b0",
+    "url": "/recipe-app/static/media/search.d36f5532.svg"
+  }
+]);
