b8a8bf9b1122f4ef562e41133498228925b02b2f
bobmatyas <ballium@protonmail.com>
2019-10-15 12:22:08 -0400

    add icons for menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+import {ReactComponent as HomeIcon} from './../images/icon-home.svg';
+import {ReactComponent as AboutIcon} from './../images/icon-about.svg';
+import {ReactComponent as RandomIcon} from './../images/icon-random.svg';
+
+const NavigationMenuContainer = styled.nav`
+  background-color: #fff;
+  border-top: 2px solid #000;
+  bottom: 0;
+  left: 0;
+  position: fixed;
+  width: 100%;
+`;
+
+const NavigationMenu = styled.ul`
+  align-items: center;
+  display: flex;
+  justify-content: space-around;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const NavigationMenuItem = styled.li`
+  margin: 0;
+  padding: 0;
+`;
 
 export const Menu = () => 
-  <nav>
-    <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/random">Random</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
-  </nav>
+  <NavigationMenuContainer>
+    <NavigationMenu>
+        <NavigationMenuItem>
+          <Link to="/">
+            <HomeIcon />
+            Home
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link to="/random">
+            <RandomIcon />
+            Random
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link to="/about">
+            <AboutIcon />
+            About
+          </Link>
+        </NavigationMenuItem>
+    </NavigationMenu>
+  </NavigationMenuContainer>
 
 export default Menu;
